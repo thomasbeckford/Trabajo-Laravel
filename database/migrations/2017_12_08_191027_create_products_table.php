fe7707c8773ed5ee2a8909d35a5b94add2becbdb
tebeck <tebeckford@gmail.com>
2017-12-08 16:37:38 -0300

    asd

---
+++ database/migrations/2017_12_08_191027_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('producto');
+            $table->decimal('precio');
+            $table->string('category');
+            $table->string('imagen');
+            $table->string('descripcion');
+            $table->decimal('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
